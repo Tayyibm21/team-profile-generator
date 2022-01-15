6fdd8848232fadd3c193a59f43f2eb764915b174
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-01-15 21:01:34 +0000

    employees test

--- team-profile-generator/src/lib/Employee.test.js
+++ team-profile-generator/src/lib/Employee.test.js
@@ -1,12 +1,33 @@
-describe('Employee', () => {
-    
-    test('should be an instance of Employee', () => {});
+const Employee = require("../src/lib/Employee");
 
-    test('should return the expected name', () => {});
-    
-    test('should return the expected id', () => {});
-    
-    test('should return the expected email', () => {});
-    
-    test('should return the expected role', () => {});
-})
+describe("Employee", () => {
+  const mockEmployee = new Employee({
+    name: "Adam Khan",
+    id: "emp6",
+    email: "AdamKham@email.com",
+  });
+
+  test("should be an instance of Employee", () => {
+    expect(mockEmployee).toBeInstanceOf(Employee);
+  });
+
+  test("should return the expected name", () => {
+    const actual = "Adam Khan";
+    expect(mockEmployee.getName()).toEqual(actual);
+  });
+
+  test("should return the expected id", () => {
+    const actual = "emp6";
+    expect(mockEmployee.getId()).toEqual(actual);
+  });
+
+  test("should return the expected email", () => {
+    const actual = "AdamKham@email.com";
+    expect(mockEmployee.getEmail()).toEqual(actual);
+  });
+
+  test("should return the role", () => {
+    const actual = "Employee";
+    expect(mockEmployee.getRole()).toEqual(actual);
+  });
+});
