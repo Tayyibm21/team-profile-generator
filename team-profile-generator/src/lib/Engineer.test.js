cabdbc86a22d660ea35c387a442f591cafc46d15
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-01-15 21:05:39 +0000

    engineer test

--- team-profile-generator/src/lib/Engineer.test.js
+++ team-profile-generator/src/lib/Engineer.test.js
@@ -1,14 +1,39 @@
-describe('Engineer', () => {
-    
-    test('should be an instance of Engineer', () => {});
+const { Engineer } = require("../src/lib");
 
-    test('should return the expected name', () => {});
-    
-    test('should return the expected id', () => {});
-    
-    test('should return the expected email', () => {});
-    
-    test('should return the expected gitHub', () => {});
-    
-    test('should return the expected role', () => {});
-})
+describe("Engineer", () => {
+  const mockEngineer = new Engineer({
+    github: "Tmontana",
+    name: "Tony montana",
+    id: "eng3",
+    email: "Tonymontata@email.com",
+  });
+
+  test("should be an instance of Engineer", () => {
+    expect(mockEngineer).toBeInstanceOf(Engineer);
+  });
+
+  test("should return the expected name", () => {
+    const actual = "Tony montana";
+    expect(mockEngineer.getName()).toEqual(actual);
+  });
+
+  test("should return the expected id", () => {
+    const actual = "eng3";
+    expect(mockEngineer.getId()).toEqual(actual);
+  });
+
+  test("should return the expected email", () => {
+    const actual = "Tonymontata@email.com";
+    expect(mockEngineer.getEmail()).toEqual(actual);
+  });
+
+  test("should return the expected github", () => {
+    const actual = "Tmontana";
+    expect(mockEngineer.getGithub()).toEqual(actual);
+  });
+
+  test("should return the role", () => {
+    const actual = "Engineer";
+    expect(mockEngineer.getRole()).toEqual(actual);
+  });
+});
