02d6f37b99d5e30aa3279d423e55923baa91fcab
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-01-15 21:08:49 +0000

    intern tests

--- team-profile-generator/src/lib/Intern.test.js
+++ team-profile-generator/src/lib/Intern.test.js
@@ -1,14 +1,39 @@
-describe('Intern', () => {
-    
-    test('should be an instance of Intern', () => {});
+const { Intern } = require("../src/lib");
 
-    test('should return the expected name', () => {});
-    
-    test('should return the expected id', () => {});
-    
-    test('should return the expected email', () => {});
-    
-    test('should return the expected school', () => {});
-    
-    test('should return the expected role', () => {});
-})
+describe("Intern", () => {
+  const mockIntern = new Intern({
+    school: "University of Birmingham",
+    name: "Aman Ahmed",
+    id: "int15",
+    email: "AmanAhmed@email.com",
+  });
+
+  test("should be an instance of Intern", () => {
+    expect(mockIntern).toBeInstanceOf(Intern);
+  });
+
+  test("should return the expected name", () => {
+    const actual = "Aman Ahmed";
+    expect(mockIntern.getName()).toEqual(actual);
+  });
+
+  test("should return the expected id", () => {
+    const actual = "int15";
+    expect(mockIntern.getId()).toEqual(actual);
+  });
+
+  test("should return the expected email", () => {
+    const actual = "AmanAhmed@email.com";
+    expect(mockIntern.getEmail()).toEqual(actual);
+  });
+
+  test("should return the expected school", () => {
+    const actual = "University of Birmingham";
+    expect(mockIntern.getSchool()).toEqual(actual);
+  });
+
+  test("should return the role", () => {
+    const actual = "Intern";
+    expect(mockIntern.getRole()).toEqual(actual);
+  });
+});
